f97d82d661147cede6731c26500a9e1a09303e00
Trident Seekers <tridentseekers@gmail.com>
2025-07-18 13:12:03 -0500

    Add timer

--- RUN1.py
+++ RUN1.py
@@ -1,5 +1,5 @@
 from pybricks.parameters import Color
-from pybricks.tools import multitask, run_task, wait
+from pybricks.tools import multitask, run_task, wait, StopWatch
 from robot_config import DRIVE_BASE, HUB, LEFT_ATTACHMENT, RIGHT_ATTACHMENT
 
 
@@ -11,12 +11,17 @@ async def run1():
     # Turn button yellow; and beep/wait so hands are out of the way
     HUB.light.on(Color.YELLOW)
     DRIVE_BASE.use_gyro(True)
-    await DRIVE_BASE.straight(100)
-    await wait(0)
+    watch = StopWatch()
+    time1 = watch.time() 
+    await DRIVE_BASE.straight(1000)
+    
+  
     # await DRIVE_BASE.straight(300)
     # await DRIVE_BASE.turn(180)
     # await DRIVE_BASE.straight(300)
     # await DRIVE_BASE.turn(-180)
     # await DRIVE_BASE.straight(-100)
     DRIVE_BASE.use_gyro(False)
+    time2 = watch.time() 
+    print(f"Time to finish: {time2 - time1} ms")
 
